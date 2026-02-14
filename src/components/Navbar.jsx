540ac9d114b12fd5a1ef4a14efef132479b30e39
m0hammadzaib <zaibfr4@gmail.com>
2026-02-14 21:17:30 +0530

    Day 2 of creating NoteBase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
-import nbLogo from '../assets/NB.png';
-import layout from '../assets/layout.png';
-import info from '../assets/info.png';
-import settings from '../assets/settings.png';
-import set from '../assets/settings.png';
+import nbLogo from '../assets/NbLogo.png';
+import grid from "../assets/grid.svg";
+import settings from "../assets/settings.svg";
+import info from "../assets/info.svg";
+import search from "../assets/search.svg";
+
 
 const Navbar = () => {
   return (
-   <div className='bg-gray-900 justify-between items-center border-b-2 border-gray-300 p-5 ps-10 flex flex-row'>
-    <div className='items-center gap-1 text-white text-3xl flex'>
+   <div className='bg-black justify-around items-center border-b-2 border-gray-300 p-5 ps-10 flex flex-row gap-20 border-b-2'>
+    <div className='items-center gap-1 text-white text-3xl flex [&>span]:font-bold'>
      <img className='h-10 w-10' src={nbLogo}/>
-      NoteBase
+     <span>Note</span><span className='text-amber-500'>Base</span>
     </div>
-    <div className='flex flex-1 justify-center'>
-      <input className='p-4 ps-7 rounded-2xl bg-gray-500 outline-0 text-white w-90 border border-gray-700' type="text" placeholder='search your notes' />
+    <div className='flex flex-1 rounded-3xl  border border-gray-700 transition-all duration-200 focus:border-amber-500 ps-2 '>
+      <img src={search} className='invert'/>
+      <input className='bg-black
+    text-white 
+    flex-1
+    px-4 
+    py-2 
+    border-none
+    outline-none ' type="text" placeholder='search notes by title or content' />
     </div>
-    <div className='flex gap-5 [&>button]:w-5 justify-around'>
-      <button><img src={layout}/></button>
-      <button><img src={info}/></button>
-      <button><img src={settings}/></button>
+    <div className='flex gap-5 [&_img]:invert [&>button]:w-5 justify-around'>
+      <button><img src={grid}/></button>
+      <button><img src={info} /></button>
+      <button><img src={settings} /></button>
     </div>
   </div>
   )
