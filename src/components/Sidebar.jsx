4b5437d0b72d04d3d8e66ea0056e14135b13483f
m0hammadzaib <zaibfr4@gmail.com>
2026-02-16 16:26:31 +0530

    Completed Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
-const Sidebar = ({ open, toggleDrawers }) => {
+const Sidebar = ({ open}) => {
   if (!open) return null;
 
   return (
-    <div className="fixed left-0 bg-transparent bg-opacity-50">
-      <div className="bg-transparent absolute left-0 top-1 w-64 h-auto p-6 text-white">
-        <button onClick={toggleDrawers} className="mb-6">
-          Close
-        </button>
-
+    <div className="fixed left-0 top-16">
+      <div className="fixed left-0 top-16 w-64 h-auto p-6 text-white">
         <ul className=" transition-all duration-1000 space-y-4 border border-white rounded-2xl p-2 bg-black [&>li]:hover:bg-amber-500 [&>li]:rounded-2xl [&>li]:p-2">
           <li>All Notes</li>
           <li>Favorites</li>
