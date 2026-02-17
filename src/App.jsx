8db6dd08e06f1e06951d026dfaff882b491faf18
m0hammadzaib <zaibfr4@gmail.com>
2026-02-17 20:31:37 +0530

    AddNote

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import NoteCard from "./components/NoteCard";
 import NotesGrid from "./components/NotesGrid";
 import Sidebar from "./components/Sidebar";
+import AddNote from "./components/AddNote";
 import { useState } from "react";
 
 
@@ -11,14 +12,20 @@ function App() {
     setopen(prev=>!prev)
   };
 
+  const handleSubmit = () =>{
+       setnotes([...notes,change])
+  };
+
   const [open, setopen] = useState(false);
   const [active, setactive] = useState(false);
   const [notes, setnotes] = useState([]);
+  const [change, setchange] = useState("");
   return (
     <div className="bg-black max-h-25">
       <Navbar toggleDrawer={()=>setopen(prev=>!prev)} />
         <Sidebar open={open} toggleDrawers={toggleDrawer}/>
-      <NotesCard notes={notes}/>
+        <AddNote add={handleSubmit} change={setchange}/>
+      <NoteCard notes={notes}/>
     </div>
   );
 }
--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
-const AddNote = () => {
+const AddNote = ({change,add}) => {
   return (
     <div>
-      
+      <div className='w-100 flex bg-black text-white border-amber-400 p-2 rounded-3xl'>
+        <input onChange={change} className='w-100 bg-black text-white outline-0 bord ' type="text" placeholder='Enter your notes'/>
+        <button className='p-3 border bg-black text-white rounded-3xl' onClick={add}>+</button>
+      </div>
     </div>
   )
 }
