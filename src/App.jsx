064b30ecefce09ce55a2b9eef6444cfda62ac353
m0hammadzaib <zaibfr4@gmail.com>
2026-02-21 17:41:10 +0530

    Added addNote logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,21 @@ import Sidebar from './components/Sidebar'
 import NotesInput from './components/NotesInput'
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
-  
+  const [notes, setNotes] = useState([
+    {id:1,title:"ZaibOne",content:"this is zaibOne notes"},
+    {id:2,title:"ZaibTwo",content:"this is zaibTwo notes"}
+  ]);
+
+  const addNote=(title,content)=>{
+     if(!title.trim() && !content.trim()) return;
+     const newNote={
+      id: Date.now(),
+         title,
+         content
+      }
+      setNotes([newNote,...notes])
+  }
+
   return (
     <>
     <div>
