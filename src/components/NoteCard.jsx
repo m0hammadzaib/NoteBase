84972668d414ac2d93d83cc3888fa306bf54d457
m0hammadzaib <zaibfr4@gmail.com>
2026-02-22 12:25:57 +0530

    Added Delete function

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const NoteCard = ({note}) => {
+const NoteCard = ({note,deleteNote}) => {
   return (
     <div>
        <div className="bg-white shadow-md rounded-xl p-4 border hover:shadow-lg transition">
       <h3 className="font-bold text-lg mb-2">{note.title}</h3>
       <p className="text-gray-700 text-sm">{note.content}</p>
+       <button onClick={() => deleteNote(note.id)}className="text-red-500 text-sm">Delete</button>
     </div>
     </div>
   )
--- src/components/NotesGrid.jsx
+++ src/components/NotesGrid.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import NoteCard from './NoteCard'
 
-const NotesGrid = ({notes}) => {
+const NotesGrid = ({notes, deleteNotes}) => {
      if (notes.length === 0) {
     return <p>No notes yet. Start by adding one!</p>;
   }
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
    {notes.map(note=>(
-      <NoteCard key={note.id} note={note} />
+      <NoteCard key={note.id} note={note} deleteNote={deleteNotes}/>
    ))}
     </div>
   )
