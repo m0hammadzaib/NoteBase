304e8660a632700bf6d67c993b5ae81e0c613923
m0hammadzaib <zaibfr4@gmail.com>
2026-03-04 00:07:01 +0530

    Changed Navbar theme styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = ({ searchTerm, setSearchTerm,setIsSettingsOpen }) => {
         <div className="w-8 h-8 rounded-md">
           <img src="https://www.svgrepo.com/show/190652/test.svg" alt="NoteBase Logo" />
         </div>
-        <h1 className="text-lg font-semibold text-[var(--text-primary)]">NoteBase</h1>
+        <h1 className="text-lg font-semibold text-[var(--text-primary)] dark:text-[var(--text-primary-dark)]">NoteBase</h1>
       </div>
 
       <input
@@ -16,12 +16,12 @@ const Navbar = ({ searchTerm, setSearchTerm,setIsSettingsOpen }) => {
         placeholder="Search your notes...."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-1/3 bg-[var(--bg-tertiary)] px-4 py-2 rounded-lg outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-1/3 bg-[var(--bg-tertiary)] dark:bg-[var(--bg-tertiary-dark)] px-4 py-2 rounded-lg outline-none focus:ring-2 focus:ring-blue-500"
       />
 
       <div className="flex items-center gap-4 relative">
         <button onClick={() => setIsSettingsOpen(true)}>
-          <img  className={`w-6 h-6  invert` } style={{ filter: "var(--icon-filter)" }} src={SettingsIcon}/>
+          <img  className={`w-6 h-6 invert`} style={{ filter: "var(--icon-filter)" }} src={SettingsIcon}/>
        </button>
       </div>
     </div>
